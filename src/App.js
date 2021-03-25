1adad666b65b52aa27fcd0a1e8ab391fc3b1164d
Artem Mezentsev <artemamway88@gmail.com>
2021-03-25 17:54:44 +0700

    Удалил лишние файлы, импортировал element в index.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, length} from './Number';
+const element = (<div>
+  <h1>Hello World!</h1>
+</div>);
 
-const flag = true;
-const name = 'World';
-const nameTwo = 'People';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style = {{
-          color: '#000',
-          fontSize: 34,
-          fontWeight: 700
-        }}>
-          Hello {flag ? name : nameTwo}!
-        </p>
-        <p>
-          {flag && 'Flag is true'}
-        </p>
-        {44 + 33}
-        {undefined}
-        {null}
-        {true}
-        {false}
-        <p>
-          {'count * length = '}
-          {count * length}
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
--- src/Number.js
+++
@@ -1,4 +0,0 @@
-const count = 1;
-const length = 979;
-
-export { count, length};
